b831a3d248122028c21f24efc05a8e45485b9996
dhacker29 <davidhackerdvm@gmail.com>
2013-08-02 21:00:11 -0500

    extract-files: Use vidc_180p.fw from mako 4.3
    Change-Id: Iff2bd303ca86db4f30d544fabff639a6285e64e6

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -314,7 +314,6 @@ LOCAL_PATH:=vendor/motorola/msm8960-common/jf
 
 # Libs from jf (Samsung Galaxy S4)
 PRODUCT_COPY_FILES += \\
-    \$(LOCAL_PATH)/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \\
     \$(LOCAL_PATH)/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \\
     \$(LOCAL_PATH)/lib/libC2D2.so:system/lib/libC2D2.so \\
     \$(LOCAL_PATH)/lib/libdivxdrm.so:system/lib/libdivxdrm.so \\
@@ -343,8 +342,11 @@ EOF
 
 LOCAL_PATH:=vendor/motorola/msm8960-common/mako/lib
 
-# Libs from mako 4.2.2 must be added from
+# libacdbloader from mako 4.2.2 must be added from
 # https://dl.google.com/dl/android/aosp/qcom-mako-jdq39-c89670ca.tgz
+# vidc_1080p.fw from 4.3 must be added from
+# https://dl.google.com/dl/android/aosp/qcom-mako-jwr66v-30ef957c.tgz
 PRODUCT_COPY_FILES += \\
+    \$(LOCAL_PATH)/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \\
     \$(LOCAL_PATH)/libacdbloader.so:system/lib/libacdbloader.so
 EOF
