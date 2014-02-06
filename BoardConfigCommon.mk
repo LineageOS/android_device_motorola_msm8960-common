22f2eec9121e3addf08ac94761430301b38f6522
dhacker29 <dhackerdvm@gmail.com>
2014-02-06 16:32:13 -0600

    Enable TARGET_USES_MSM_30_KERNEL := true to fix recovery compile
    Change-Id: Iac0f84dd9be13f6936a290a6ae3dbe0a2bfc87f5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,6 +73,7 @@ BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
 TARGET_OTA_ASSERT_DEVICE := xt925,xt926,xt907,vanquish_u,vanquish,scorpion_mini,mb886,qinara,asanti,asanti_c,xt897,xt897c
 
 # Recovery
+TARGET_USES_MSM_30_KERNEL := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 BOARD_RECOVERY_SWIPE := true
